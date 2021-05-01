9d52c7c2269916bc410a709a1c284cfd17a8eccd
PCpc8002828 <2042079055@qq.com>
2021-05-01 11:21:43 +0800

    新增User实体类

---
+++ src/main/java/com/zangyi/common/User.java
@@ -0,0 +1,53 @@
+package com.zangyi.common;
+
+public class User {
+    private Integer id;
+
+    private String userName;
+
+    private String userPassword;
+
+    private Integer userAge;
+
+    private String userSer;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword == null ? null : userPassword.trim();
+    }
+
+    public Integer getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(Integer userAge) {
+        this.userAge = userAge;
+    }
+
+    public String getUserSer() {
+        return userSer;
+    }
+
+    public void setUserSer(String userSer) {
+        this.userSer = userSer == null ? null : userSer.trim();
+    }
+}
