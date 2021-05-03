18cd5874115e1fd9626bf83efcaeae15c53b5762
PCpc8002828 <2042079055@qq.com>
2021-05-03 20:44:29 +0800

    cj1

--- src/main/java/com/zangyi/controller/UserController.java
+++ src/main/java/com/zangyi/controller/UserController.java
@@ -1,9 +1,27 @@
 package com.zangyi.controller;
 
+import com.zangyi.common.User;
+import com.zangyi.common.UserExample;
+import com.zangyi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    /**
+     * 作者：普尺
+     */
+//    插入数据：
+    @Autowired
+    UserService userService;
+    @ResponseBody
+    @RequestMapping("/insert")
+    public int userInsert(){
+        return userService.userInsert();
+    }
+
+
 }
